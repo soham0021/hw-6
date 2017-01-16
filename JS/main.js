ebc4f742cc82a9bbf9c2ca6c7611810cd4ec42f0
soham patel <sohampatel9055@yahoo.com>
2017-01-16 14:31:02 -0500

    done with first set of objactiv

---
+++ JS/main.js
@@ -0,0 +1,20 @@
+// getting the id of two button
+
+var button1 = document.getElementById('click1')
+// adding event listner
+click1.addEventListener("click" , function() {
+
+	//getting h1 by its id
+	var head= document.getElementById('foo')
+	foo.innerHTML = "<h1> I'm right</h1>"
+})
+
+var button2=document.getElementById('click2')
+// add event listner
+click2.addEventListener("click" , function(){
+	// getting h1 by its id 
+	var head2=document.getElementById("foo")
+	// changing the innerHTML
+	foo.innerHTML="<h1> No.. I'm right!"
+
+})
